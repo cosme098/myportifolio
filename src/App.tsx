a3708408168f8e5e91f3dce8e51ffd0c3fced78e
cosme098 <cosmeforeve@gmail.com>
2022-02-28 08:50:46 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Home from './components/home';
+import Header from './components/header';
+import SocialMidias from './components/social-midias';
+import About from './components/About/about';
+import Projects from './components/projects/index';
+import Skills from './components/skills/index';
+import Footer from './components/footer/index';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home/>
+      <SocialMidias/> 
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
